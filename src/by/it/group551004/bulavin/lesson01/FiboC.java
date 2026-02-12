315cca94f6d447690ce36470a15aee8307618af2
JustiZzZz <bulavinkirill02@gmail.com>
2026-02-12 17:11:44 +0300

    Lesson01-Done-First-Second-And-Half-Third-Tasks

--- src/by/it/group551004/bulavin/lesson01/FiboC.java
+++ src/by/it/group551004/bulavin/lesson01/FiboC.java
@@ -6,6 +6,8 @@ package by.it.group551004.bulavin.lesson01;
  * время расчета должно быть не более 2 секунд
  */
 
+import java.math.BigInteger;
+
 public class FiboC {
 
     private long startTime = System.currentTimeMillis();
@@ -22,11 +24,44 @@ public class FiboC {
     }
 
     long fasterC(long n, int m) {
-        //Интуитивно найти решение не всегда просто и
-        //возможно потребуется дополнительный поиск информации
-        return -1L;
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+
+        int prev = 0;
+        int curr = 1;
+        int period = 0;
+
+        /*
+         * Почитал, что такое период Пизано
+         * и чекнул как люди накидывают алгоритм.
+         * Постарался свою версию представить.
+         * Был вариант написать через перемножение матриц,
+         * Но я не математик. Сам к заданию полностью не додумался.
+         */
+
+        for (int i = 0; i < m * 6; i++) {
+            int temp = (prev + curr) % m;
+            prev = curr;
+            curr = temp;
+
+            if (prev == 0 && curr == 1) {
+                period = i + 1;
+            }
+        }
+
+        long reduced_n = n % period;
+
+        long a = 0;
+        long b = 1;
+        long answer = 0;
+
+        for (int i = 2; i <= reduced_n; i++) {
+            answer = (a + b) % m;
+            a = b;
+            b = answer;
+        }
+
+        return answer;
     }
-
-
 }
 
