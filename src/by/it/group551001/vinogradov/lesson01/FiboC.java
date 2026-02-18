4cd30f9d1d1ffa1f6e0a4daf8cc93560707cbe6b
DolphyXSTD <timofey31052008@gmail.com>
2026-02-18 17:34:27 +0300

    Task C done

--- src/by/it/group551001/vinogradov/lesson01/FiboC.java
+++ src/by/it/group551001/vinogradov/lesson01/FiboC.java
@@ -22,9 +22,31 @@ public class FiboC {
     }
 
     long fasterC(long n, int m) {
-        //Интуитивно найти решение не всегда просто и
-        //возможно потребуется дополнительный поиск информации
-        return -1L;
+
+        long prev = 0;
+        long curr = 1;
+        long p = 0;
+
+        for (int i = 0; i < m * 6; i++) {
+            long temp = curr;
+            curr = (prev + curr) % m;
+            prev = temp;
+
+            if (prev == 0 && curr == 1) {
+                p = i + 1;
+                break;
+            }
+        }
+        long r = n % p;
+        prev = 0;
+        curr = 1;
+
+        for (long i = 0; i < r; i++) {
+            long temp = curr;
+            curr = (prev + curr) % m;
+            prev = temp;
+        }
+        return prev;
     }
 
 
