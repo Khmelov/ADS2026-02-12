5e1a734ca5e7459c4ab3eff0bc96a6f37232d25c
DolphyXSTD <timofey31052008@gmail.com>
2026-02-25 19:16:33 +0300

    Task C done with mergeSort (lesson 2)

--- src/by/it/group551001/vinogradov/lesson02/C_GreedyKnapsack.java
+++ src/by/it/group551001/vinogradov/lesson02/C_GreedyKnapsack.java
@@ -40,19 +40,72 @@ public class C_GreedyKnapsack {
         }
         System.out.printf("Всего предметов: %d. Рюкзак вмещает %d кг.\n", n, W);
 
-        //тут необходимо реализовать решение задачи
-        //итогом является максимально воможная стоимость вещей в рюкзаке
-        //вещи можно резать на кусочки (непрерывный рюкзак)
+        mergeSort(items, 0, n-1);
         double result = 0;
-        //тут реализуйте алгоритм сбора рюкзака
-        //будет особенно хорошо, если с собственной сортировкой
-        //кроме того, можете описать свой компаратор в классе Item
-
-        //ваше решение.
+        for (Item item : items) {
+            if (item.weight <= W) {
+                W -= item.weight;
+                result += item.cost;
+            }
+            else {
+                result += (double) W / item.weight * item.cost;
+                break;
+            }
+        }
 
 
         System.out.printf("Удалось собрать рюкзак на сумму %f\n", result);
         return result;
+    }
+
+    private void mergeSort(Item[] items, int l, int r) {
+        if (l < r) {
+            int m = l + (r - l) / 2;
+            mergeSort(items, l, m);
+            mergeSort(items, m + 1, r);
+            merge(items, l, m, r);
+        }
+    }
+
+    private void merge(Item[] items, int l, int m, int r) {
+        int n1 = m - l + 1;
+        int n2 = r - m;
+
+        Item[] left = new Item[n1];
+        Item[] right = new Item[n2];
+
+        for (int i = 0; i < n1; i++) {
+            left[i] = items[l + i];
+        }
+        for (int j = 0; j < n2; j++) {
+            right[j] = items[m + 1 + j];
+        }
+
+        int i = 0, j = 0;
+        int k = l;
+
+        while (i < n1 && j < n2) {
+            if ((double) left[i].cost / left[i].weight >= (double) right[j].cost / right[j].weight) {
+                items[k] = left[i];
+                i++;
+            } else {
+                items[k] = right[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < n1) {
+            items[k] = left[i];
+            i++;
+            k++;
+        }
+
+        while (j < n2) {
+            items[k] = right[j];
+            j++;
+            k++;
+        }
     }
 
     private static class Item implements Comparable<Item> {
