cb503a970fa1ae197ec77fb6fa3ca5d5f7bae6e0
Bolbas <bolbas.kseniya@mail.ru>
2026-02-27 13:50:12 +0300

    lesson1

--- src/by/it/group510901/bolbas/lesson01/FiboA.java
+++ src/by/it/group510901/bolbas/lesson01/FiboA.java
@@ -28,16 +28,18 @@ public class FiboA {
         return res;
     }
 
-    private int calc(int n) {
-        n = 10;
-        long[] fib = new long[n];
+    private long calc(int n) {
+        if (n <= 1) return n;
+
+        long[] fib = new long[n + 1];
         fib[0] = 0;
         fib[1] = 1;
-        for (int i = 2; i < n; i++) {
+
+        for (int i = 2; i <= n; i++) {
             fib[i] = fib[i - 1] + fib[i - 2];
         }
-// Вывод: 0 1 1 2 3 5 8 13 21 34
-        return 0;
+
+        return fib[n];
     }
 
 
