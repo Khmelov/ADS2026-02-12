65cf596c82f32912e74a8032675ec8ae43304bfb
hannakakhno <hannakakhno@gmail.com>
2026-03-04 21:44:25 +0300

    lesson1 a b c

--- src/by/it/group551003/kakhno/lesson01/FiboC.java
+++ src/by/it/group551003/kakhno/lesson01/FiboC.java
@@ -24,7 +24,21 @@ public class FiboC {
     long fasterC(long n, int m) {
         //Интуитивно найти решение не всегда просто и
         //возможно потребуется дополнительный поиск информации
-        return -1L;
+        if (n < 2) return n;
+        int pisanoPeriod = 0, lim = 6 * m, size = lim + 2; // п(m)<=6m - период Пизано не превышает 6m
+        int[] pisanoArr = new int[size];
+        pisanoArr[0] = 0;
+        pisanoArr[1] = 1;
+        for (int i = 2; i < lim; i++) {
+            pisanoArr[i] = (pisanoArr[i - 1] + pisanoArr[i - 2]) % m;
+            if (pisanoArr[i - 1] == 0 && pisanoArr[i] == 1) { // нумерация с нуля, если iый эл-т =0(новое начало, новый нулевой), то чисел в периоде i
+                pisanoPeriod = i - 1;
+                break;
+            }
+        }
+        // F[n] % m = F[n % п[m]] % m
+        int reducedN = (int)(n % pisanoPeriod); // (int) - преобразуем long в int
+        return pisanoArr[reducedN];
     }
 
 
