b26fa9e4dcd3db4e82f646bbba124665e093788e
anastasia karpovich <anstasia.karpovichch2007@gmail.com>
2026-03-05 21:24:07 +0300

    done-lesson2-all

--- src/by/it/group551004/karpovich/lesson02/B_Sheduler.java
+++ src/by/it/group551004/karpovich/lesson02/B_Sheduler.java
@@ -1,6 +1,8 @@
 package lesson02;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 /*
 Даны интервальные события events
@@ -32,7 +34,15 @@ public class B_Sheduler {
         List<Event> result;
         result = new ArrayList<>();
         //ваше решение.
+        Arrays.sort(events, Comparator.comparingInt(e -> e.stop));
+        int lastEndTime = from;
 
+        for (Event event : events) {
+            if (event.start >= lastEndTime) {
+                result.add(event);
+                lastEndTime = event.stop;  // обновляем время окончания
+            }
+        }
 
         return result;          //вернем итог
     }
