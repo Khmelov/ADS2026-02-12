46a10351f15b136362f598cf72bbea9578be22d4
belozorchik <0little0nightmare0@gmail.com>
2026-03-14 21:05:19 +0300

    task c done

--- src/by/it/group551001/belozorchik/lesson01/FiboC.java
+++ src/by/it/group551001/belozorchik/lesson01/FiboC.java
@@ -24,9 +24,43 @@ public class FiboC {
     long fasterC(long n, int m) {
         //Интуитивно найти решение не всегда просто и
         //возможно потребуется дополнительный поиск информации
-        return -1L;
+        if (n == 0 || n == 1)
+            return n;
+        if (m == 1)
+            return 0;
+
+        long x1 = 0;
+        long x2 = 1;
+        long x3;
+        int ostx1 = 0;
+        int ostx2 = 1; // начинаю с пары (0,1)
+        int period = 0; // это длина периода
+
+        // нахожу период
+        do {
+            x3 = x1 + x2;
+            x1 = x2;
+            x2 = x3;
+            ostx1 = (int)(x1 % m);
+            ostx2 = (int)(x2 % m);
+            period++;
+        } while (!(ostx1 == 0 && ostx2 == 1)); // пока не найдена (0,1) снова
+
+        // period содержит длину периода Пизано
+        // нахожу индекс k
+        int k = (int)(n % period);
+
+        // вычисляю F катое и возвращаю остаток
+        if (k == 0) return 0;
+        if (k == 1) return 1;
+
+        long a = 0, b = 1, c = 0;       //где а = F(0), b = F(1)
+        for (int i = 2; i <= k; i++) {
+            c = a + b;
+            a = b;
+            b = c;
+        }
+        return c % m;
     }
-
-
 }
 
