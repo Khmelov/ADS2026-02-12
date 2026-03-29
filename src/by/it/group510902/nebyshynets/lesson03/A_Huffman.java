7ed59271d94365260aeba5351f4102c6512b26e5
MichaelNebyshynets <nebyshynets.m@gmail.com>
2026-03-29 14:19:42 +0300

    Lesson-03-Task-A-Done

--- src/by/it/group510902/nebyshynets/lesson03/A_Huffman.java
+++ src/by/it/group510902/nebyshynets/lesson03/A_Huffman.java
@@ -68,9 +68,16 @@ public class A_Huffman {
         Map<Character, Integer> count = new HashMap<>();
         //1. переберем все символы по очереди и рассчитаем их частоту в Map count
         //для каждого символа добавим 1 если его в карте еще нет или инкремент если есть.
-
+        for(int i = 0; i<s.length();i++){
+            char c = s.charAt(i);
+            count.put(c, count.getOrDefault(c, 0) + 1);
+        }
         //2. перенесем все символы в приоритетную очередь в виде листьев
         PriorityQueue<Node> priorityQueue = new PriorityQueue<>();
+
+        for (Map.Entry<Character, Integer> entry : count.entrySet()) {
+            priorityQueue.add(new LeafNode(entry.getValue(), entry.getKey()));
+        }
 
         //3. вынимая по два узла из очереди (для сборки родителя)
         //и возвращая этого родителя обратно в очередь
@@ -79,8 +86,25 @@ public class A_Huffman {
 
         //4. последний из родителей будет корнем этого дерева
         //это будет последний и единственный элемент оставшийся в очереди priorityQueue.
+
+        if(priorityQueue.size()==1){
+            Node leaf = priorityQueue.poll();
+            leaf.fillCodes("0");
+        }else{
+            while(priorityQueue.size()>1){
+                Node left = priorityQueue.poll();
+                Node right = priorityQueue.poll();
+                InternalNode parent = new InternalNode(left, right);
+                priorityQueue.add(parent);
+            }
+            if(!priorityQueue.isEmpty()) priorityQueue.poll().fillCodes("");
+        }
+
         StringBuilder sb = new StringBuilder();
         //.....
+        for(int i = 0; i<s.length();i++){
+            sb.append(codes.get(s.charAt(i)));
+        }
 
         return sb.toString();
         //01001100100111
