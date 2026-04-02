2da6c52cfe822de611b6083a6afea41cc1f460d2
DolphyXSTD <timofey31052008@gmail.com>
2026-04-02 14:16:30 +0300

    Task A and C done (lesson 5)

--- src/by/it/group551001/vinogradov/lesson05/A_QSort.java
+++ src/by/it/group551001/vinogradov/lesson05/A_QSort.java
@@ -52,22 +52,29 @@ public class A_QSort {
         //число отрезков отсортированного массива
         int n = scanner.nextInt();
         Segment[] segments = new Segment[n];
+        int[] starts = new int[n];
+        int[] stops = new int[n];
         //число точек
         int m = scanner.nextInt();
         int[] points = new int[m];
         int[] result = new int[m];
-
-        //читаем сами отрезки
         for (int i = 0; i < n; i++) {
             //читаем начало и конец каждого отрезка
             segments[i] = new Segment(scanner.nextInt(), scanner.nextInt());
+            starts[i] = segments[i].start;
+            stops[i] = segments[i].stop;
         }
+
+        quickSort(starts, 0, n - 1);
+        quickSort(stops, 0, n - 1);
+
         //читаем точки
         for (int i = 0; i < m; i++) {
             points[i] = scanner.nextInt();
+            int countStart = count(starts, points[i] + 1);
+            int countStop = count(stops, points[i]);
+            result[i] = countStart - countStop;
         }
-        //тут реализуйте логику задачи с применением быстрой сортировки
-        //в классе отрезка Segment реализуйте нужный для этой задачи компаратор
 
 
         //!!!!!!!!!!!!!!!!!!!!!!!!!     КОНЕЦ ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
@@ -80,10 +87,13 @@ public class A_QSort {
         int stop;
 
         Segment(int start, int stop) {
+            if (start > stop) {
+                int temp = stop;
+                stop = start;
+                start = temp;
+            }
             this.start = start;
             this.stop = stop;
-            //тут вообще-то лучше доделать конструктор на случай если
-            //концы отрезков придут в обратном порядке
         }
 
         @Override
@@ -93,5 +103,39 @@ public class A_QSort {
             return 0;
         }
     }
+    private int count(int[] arr, int value) {
+        int l = 0, r = arr.length - 1;
+        int cnt = 0;
+        while (l <= r) {
+            int m = (l + r) / 2;
+            if (arr[m] < value) {
+                cnt = m + 1;
+                l = m + 1;
+            } else {
+                r = m - 1;
+            }
+        }
+        return cnt;
+    }
 
+    private void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int p = partition(arr, low, high);
+            quickSort(arr, low, p - 1);
+            quickSort(arr, p + 1, high);
+        }
+    }
+
+    private int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = low;
+        for (int j = low; j < high; j++) {
+            if (arr[j] <= pivot) {
+                int temp = arr[i]; arr[i] = arr[j]; arr[j] = temp;
+                i++;
+            }
+        }
+        int temp = arr[i]; arr[i] = arr[high]; arr[high] = temp;
+        return i;
+    }
 }
