0be1092d290e05ee751fee092a2567937568f922
NiksNewgames <niksnewgames@gmail.com>
2026-04-08 16:55:11 +0300

    lesson05

--- src/by/it/group551004/sheev/lesson05/A_QSort.java
+++ src/by/it/group551004/sheev/lesson05/A_QSort.java
@@ -31,7 +31,6 @@ import java.util.Scanner;
     1 6 11
     Sample Output:
     1 0 0
-
 */
 
 public class A_QSort {
@@ -46,35 +45,87 @@ public class A_QSort {
     }
 
     int[] getAccessory(InputStream stream) throws FileNotFoundException {
-        //подготовка к чтению данных
         Scanner scanner = new Scanner(stream);
-        //!!!!!!!!!!!!!!!!!!!!!!!!!     НАЧАЛО ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
-        //число отрезков отсортированного массива
+
         int n = scanner.nextInt();
         Segment[] segments = new Segment[n];
-        //число точек
         int m = scanner.nextInt();
         int[] points = new int[m];
         int[] result = new int[m];
 
-        //читаем сами отрезки
         for (int i = 0; i < n; i++) {
-            //читаем начало и конец каждого отрезка
-            segments[i] = new Segment(scanner.nextInt(), scanner.nextInt());
+            int start = scanner.nextInt();
+            int stop = scanner.nextInt();
+            segments[i] = new Segment(Math.min(start, stop), Math.max(start, stop));
         }
-        //читаем точки
+
         for (int i = 0; i < m; i++) {
             points[i] = scanner.nextInt();
         }
-        //тут реализуйте логику задачи с применением быстрой сортировки
-        //в классе отрезка Segment реализуйте нужный для этой задачи компаратор
 
+        // Сортировка сегментов с помощью быстрой сортировки
+        quickSort(segments, 0, segments.length - 1);
 
-        //!!!!!!!!!!!!!!!!!!!!!!!!!     КОНЕЦ ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
+        // Для каждой точки посчитайте, к скольким сегментам она принадлежит
+        for (int i = 0; i < m; i++) {
+            int point = points[i];
+            int count = 0;
+
+            // Бинарный поиск для нахождения первого сегмента, который может содержать точку
+            int left = 0, right = n - 1;
+            while (left <= right) {
+                int mid = left + (right - left) / 2;
+                if (segments[mid].stop < point) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+
+            // Подсчет сегментов, содержащих точку
+            for (int j = left; j < n; j++) {
+                if (segments[j].start > point) {
+                    break;
+                }
+                if (segments[j].start <= point && point <= segments[j].stop) {
+                    count++;
+                }
+            }
+
+            result[i] = count;
+        }
+
         return result;
     }
 
-    //отрезок
+    private void quickSort(Segment[] arr, int low, int high) {
+        if (low < high) {
+            int pi = partition(arr, low, high);
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+        }
+    }
+
+    private int partition(Segment[] arr, int low, int high) {
+        Segment pivot = arr[high];
+        int i = low - 1;
+
+        for (int j = low; j < high; j++) {
+            if (arr[j].compareTo(pivot) <= 0) {
+                i++;
+                Segment temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+
+        Segment temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+
+        return i + 1;
+    }
+
     private class Segment implements Comparable<Segment> {
         int start;
         int stop;
@@ -82,16 +133,15 @@ public class A_QSort {
         Segment(int start, int stop) {
             this.start = start;
             this.stop = stop;
-            //тут вообще-то лучше доделать конструктор на случай если
-            //концы отрезков придут в обратном порядке
         }
 
         @Override
         public int compareTo(Segment o) {
-            //подумайте, что должен возвращать компаратор отрезков
-
-            return 0;
+            // Сортировка по начальной координате, затем по конечной координате
+            if (this.start != o.start) {
+                return Integer.compare(this.start, o.start);
+            }
+            return Integer.compare(this.stop, o.stop);
         }
     }
-
 }
