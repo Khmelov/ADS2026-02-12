97fc1c3e3cdb0ffe245337e935e039eb0ec47a15
alexandrpuchinets-glitch <alexandr.puchinets@gmail.com>
2026-04-26 00:53:53 +0300

    lesson6 B

--- src/by/it/group551004/puchinets/lesson06/B_LongDivComSubSeq.java
+++ src/by/it/group551004/puchinets/lesson06/B_LongDivComSubSeq.java
@@ -49,8 +49,21 @@ public class B_LongDivComSubSeq {
             m[i] = scanner.nextInt();
         }
         //тут реализуйте логику задачи методами динамического программирования (!!!)
+        int[] d = new int[n];
         int result = 0;
 
+        for (int i = 0; i < n; i++) {
+            d[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (m[i] % m[j] == 0 && d[j] + 1 > d[i]) {
+                    d[i] = d[j] + 1;
+                }
+            }
+            // Обновляем общий максимум
+            if (d[i] > result) {
+                result = d[i];
+            }
+        }
 
         //!!!!!!!!!!!!!!!!!!!!!!!!!     КОНЕЦ ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
         return result;
