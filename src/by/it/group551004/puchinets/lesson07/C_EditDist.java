0422d4a6fbdc246e28a6b55636823c235412c8eb
alexandrpuchinets-glitch <alexandr.puchinets@gmail.com>
2026-04-26 01:02:55 +0300

    lesson7 C

--- src/by/it/group551004/puchinets/lesson07/C_EditDist.java
+++ src/by/it/group551004/puchinets/lesson07/C_EditDist.java
@@ -51,10 +51,53 @@ public class C_EditDist {
     String getDistanceEdinting(String one, String two) {
         //!!!!!!!!!!!!!!!!!!!!!!!!!     НАЧАЛО ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
 
-
-        String result = "";
+        int n = one.length(), m = two.length();
+        int[][] dp = new int[n + 2][m + 2];
+        for(int i = 0; i <= n + 1; i++){
+            for(int j = 0; j <= m + 1; j++){
+                dp[i][j] = (int)2e9;
+            }
+        }
+        dp[n][m] = 0;
+        for(int i = n; i >= 0; i--){
+            for(int j = m; j >= 0; j--) {
+                if(n == i && m == j){
+                    continue;
+                }
+                if(n == i){
+                    dp[i][j] = dp[i][j + 1] + 1;
+                    continue;
+                }
+                if(m == j){
+                    dp[i][j] = dp[i + 1][j] + 1;
+                    continue;
+                }
+                dp[i][j] = (dp[i + 1][j] < dp[i][j + 1] ? dp[i + 1][j] : dp[i][j + 1]) + 1;
+                int x = dp[i + 1][j + 1] + (one.charAt(i) != two.charAt(j) ? 1 : 0);
+                dp[i][j] = (dp[i][j] < x ? dp[i][j] : x);
+            }
+        }
+        StringBuilder result = new StringBuilder();
+        int x = 0, y = 0;
+        while(x < n || y < m){
+            if(x < n && y < m && one.charAt(x) == two.charAt(y)){
+                result.append("#,");
+                x++;
+                y++;
+            } else if(x < n && y < m && dp[x][y] == dp[x + 1][y + 1] + 1){
+                result.append("~").append(two.charAt(y)).append(",");
+                x++;
+                y++;
+            } else if(y < m && dp[x][y] == dp[x][y + 1] + 1){
+                result.append("+").append(two.charAt(y)).append(",");
+                y++;
+            } else if(x < n && dp[x][y] == dp[x + 1][y] + 1){
+                result.append("-").append(one.charAt(x)).append(",");
+                x++;
+            }
+        }
         //!!!!!!!!!!!!!!!!!!!!!!!!!     КОНЕЦ ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
-        return result;
+        return result.toString();
     }
 
 
