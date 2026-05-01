58a8e2bb082c83ec76572934d5bcc8ee2731504f
RecursiveKing <hostinginformationishere@gmail.com>
2026-05-01 18:22:38 +0300

    ADD lesson 07, A ready

--- src/by/it/group551001/bondarenko/lesson07/A_EditDist.java
+++ src/by/it/group551001/bondarenko/lesson07/A_EditDist.java
@@ -41,13 +41,33 @@ public class A_EditDist {
 
     int getDistanceEdinting(String one, String two) {
         //!!!!!!!!!!!!!!!!!!!!!!!!!     НАЧАЛО ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
+        int[][] D = new int[one.length() + 1][two.length() + 1];
+        for(int i = 0; i <= one.length(); i++) {
+            for(int j = 0; j <= two.length(); j++) {
+                D[i][j] = Integer.MAX_VALUE;
+            }
+        }
 
-
-        int result = 0;
+        int result = editDist(one.length(), two.length(), one, two, D);
         //!!!!!!!!!!!!!!!!!!!!!!!!!     КОНЕЦ ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
         return result;
     }
-
+    int editDist(int i, int j, String one, String two, int[][] D) {
+        if (i == 0) return j;
+        if (j == 0) return i;
+        if (D[i][j] != Integer.MAX_VALUE) {
+            return D[i][j];
+        }
+        if (one.charAt(i - 1) == two.charAt(j - 1)) {
+            D[i][j] = editDist(i - 1, j - 1, one, two, D);
+        } else {
+            int ins = editDist(i, j - 1, one, two, D);
+            int del = editDist(i - 1, j, one, two, D);
+            int rep = editDist(i - 1, j - 1, one, two, D);
+            D[i][j] = 1 + Math.min(Math.min(ins, del), rep);
+        }
+        return D[i][j];
+    }
 
     public static void main(String[] args) throws FileNotFoundException {
         InputStream stream = A_EditDist.class.getResourceAsStream("dataABC.txt");
