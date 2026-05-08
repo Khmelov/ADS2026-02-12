5c717ced2290f2bd4d16671ad4a4c11503711746
demyanj <rusilevich.demyan@icloud.com>
2026-05-08 18:47:50 +0300

    lessons 1-5

--- src/by/it/group510901/rusilevich/lesson01/FiboB.java
+++ src/by/it/group510901/rusilevich/lesson01/FiboB.java
@@ -1,4 +1,4 @@
-package by.it.group510901.rusilevich;
+package by.it.group510901.rusilevich.lesson01;
 
 import java.math.BigInteger;
 
@@ -12,7 +12,6 @@ public class FiboB {
     private long startTime = System.currentTimeMillis();
 
     public static void main(String[] args) {
-        //вычисление чисел простым быстрым методом
         FiboB fibo = new FiboB();
         int n = 55555;
         System.out.printf("fastB(%d)=%d \n\t time=%d \n\n", n, fibo.fastB(n), fibo.time());
@@ -23,15 +22,19 @@ public class FiboB {
     }
 
     BigInteger fastB(Integer n) {
-        //здесь нужно реализовать вариант с временем O(n) и памятью O(n)
-        if (n == 0) return BigInteger.ZERO;
-        if (n == 1) return BigInteger.ONE;
+        if (n < 0) {
+            throw new IllegalArgumentException("n не может быть отрицательным");
+        }
+        if (n <= 1) {
+            return BigInteger.valueOf(n);
+        }
         BigInteger[] fib = new BigInteger[n + 1];
         fib[0] = BigInteger.ZERO;
         fib[1] = BigInteger.ONE;
         for (int i = 2; i <= n; i++) {
             fib[i] = fib[i - 1].add(fib[i - 2]);
         }
+
         return fib[n];
     }
 
